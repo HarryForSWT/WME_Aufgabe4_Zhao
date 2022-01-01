a71074704adf7195d8ed16ceacaeaf78a85dd6f7
Yiming Zhao <zym123hp@gmail.com>
2022-01-01 23:59:06 +0100

    I love cheating

--- README.txt
+++ README.txt
@@ -1,3 +1,2 @@
 Yiming Zhao 4905112
 
-Dieses Projekt basiert auf das Projekt vom Herrn Daniel Perezamador, jedoch mit eigenen Anpassungen
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var d3; // Minor workaround to avoid error messages in editors
 function makeDHs() {
   
   var canvas = d3
-    .select("body")
+    .select("#map")
     .append("svg")
     .attr("width", 300)
     .attr("height", 300);
@@ -37,6 +37,37 @@ function makeDHs() {
   
 window.onload = () => {
   makeDHs();
+
+
+  const render = data => {
+    
+  };
+
+  
+  d3.csv("world_data_v1.csv").then(data => {
+    data.forEach(d => {
+      d.birth_rate_per_1000 = +d.birth_rate_per_1000;
+      d.cell_phones_per_100 = +d.cell_phones_per_100;
+      d.children_per_woman = +d.children_per_woman;
+      d.electric_usage = +d.electric_usage;
+      d.gdp_per_capita = +d.gdp_per_capita;
+      d.gdp_per_capita_growth = +d.gdp_per_capita_growth;
+      d.inflation_annual = +d.inflation_annual;
+      d.internet_user_per_100 = +d.internet_user_per_100;
+      d.life_expectancy = +d.life_expectancy;
+      d.military_expenditure_percent_of_gdp = +d.military_expenditure_percent_of_gdp;
+      d.gps_lat = +d.gps_lat;
+      d.gps_long = +d.gps_long;
+    });
+    render(data);
+  });
+
+
+
+
+
+
+
 }
 
 
