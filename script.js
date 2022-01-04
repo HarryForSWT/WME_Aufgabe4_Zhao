1990a45d9f29cfd128a73ac6c11c29fdfb45fb3f
Yiming Zhao <zym123hp@gmail.com>
2022-01-04 01:46:39 +0100

    1. BarChart fast fertig, außer negativen Werten

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 var d3; // Minor workaround to avoid error messages in editors
 
 window.onload = () => {
-  makeDHs();
+  makeDHs();//Test funktion
+  var database;
+  const keys=[
+    "birth_rate_per_1000",
+    "cell_phones_per_100",
+    "children_per_woman",
+    "electric_usage",
+    "gdp_per_capita",
+    "gdp_per_capita_growth",
+    "inflation_annual",
+    "internet_user_per_100",
+    "life_expectancy",
+    "military_expenditure_percent_of_gdp"
+  ];
+  lastSelected = keys[0];
+  
   
   var svg1 = d3.select("#svg1");
   const svg1height= +svg1.attr("height") ;
@@ -10,11 +25,69 @@ window.onload = () => {
   const svg2height= +svg2.attr("height") ;
   const svg2width= +svg2.attr("width");
 
+  d3.csv("world_data_v1.csv").then(data => {
+    data.forEach(d => {
+      d.birth_rate_per_1000 = +d.birth_rate_per_1000;
+      d.cell_phones_per_100 = +d.cell_phones_per_100;
+      d.children_per_woman = +d.children_per_woman;
+      d.electric_usage = +d.electric_usage;
+      d.gdp_per_capita = +d.gdp_per_capita;
+      d.gdp_per_capita_growth = +d.gdp_per_capita_growth;
+      d.inflation_annual = +d.inflation_annual;
+      d.internet_user_per_100 = +d.internet_user_per_100;
+      d.life_expectancy = +d.life_expectancy;
+      d.military_expenditure_percent_of_gdp = +d.military_expenditure_percent_of_gdp;
+      d.gps_lat = +d.gps_lat;
+      d.gps_long = +d.gps_long;
+    });
+    database = data;
+    render(data);
+  });
+
 
 function render(data){
-    
+    d3.select("#svg1").selectAll("rect").remove();
+    d3.selectAll("#yAxis").remove();
+    var result;
     const xValue = d => d.name;
-    const yValue = d=> d.birth_rate_per_1000;
+    var yValue =function (d) {
+      switch(lastSelected){
+        case "birth_rate_per_1000" :
+          result= d.birth_rate_per_1000;
+          break;
+        case "cell_phones_per_100":
+          result= d.cell_phones_per_100;
+          break;
+        case "children_per_woman":
+          result= d.children_per_woman;
+          break;
+        case "electric_usage":
+          result= d.electric_usage;
+          break;
+        case "gdp_per_capita":
+          result= d.gdp_per_capita;
+          break;
+        case "gdp_per_capita_growth":
+          result= d.gdp_per_capita_growth;
+          break;
+        case "inflation_annual":
+          result= d.inflation_annual;
+          break;
+        case "internet_user_per_100":
+          result= d.internet_user_per_100;
+          break;
+        case "life_expectancy":
+          result= d.life_expectancy;
+          break;
+        case "military_expenditure_percent_of_gdp":
+          result= d.military_expenditure_percent_of_gdp;
+          break;
+        default:
+          break;
+      }
+      return result;
+    };
+
 
     //y "Wertebereich" dateneinstellen
     const yScale = d3
@@ -22,6 +95,7 @@ function render(data){
       .domain([0, d3.max(data, yValue)])
       .nice()
       .range([150,0]);
+
     //x "Definitionsbereich" Dateneinstellen
     const xScale = d3
       .scaleBand()
@@ -40,6 +114,7 @@ function render(data){
     const yAxisG1 = g1
       .append("g")
       .call(yAxis)
+      .attr("id","yAxis")
       .attr("transform", `translate(40,30)`);
    
     //x-Achse Zeichnen
@@ -67,25 +142,14 @@ function render(data){
   };
 
 
-  const keys=[
-    "birth_rate_per_1000",
-    "cell_phones_per_100",
-    "children_per_woman",
-    "electric_usage",
-    "gdp_per_capita",
-    "gdp_per_capita_growth",
-    "inflation_annual",
-    "internet_user_per_100",
-    "life_expectancy",
-    "military_expenditure_percent_of_gdp"
-  ];
+  
 
   const option1 = d3.select("#bar1")
     .insert("select", ":first-child")
     .attr("id", "select1")
     .on("change", function () {
       lastSelected = this.value;
-      console.log(lastSelected);
+      render(database);
     })
     .selectAll("option")
     .data(keys)
@@ -104,27 +168,10 @@ function render(data){
  
     });
 
-  d3.csv("world_data_v1.csv").then(data => {
-    data.forEach(d => {
-      d.birth_rate_per_1000 = +d.birth_rate_per_1000;
-      d.cell_phones_per_100 = +d.cell_phones_per_100;
-      d.children_per_woman = +d.children_per_woman;
-      d.electric_usage = +d.electric_usage;
-      d.gdp_per_capita = +d.gdp_per_capita;
-      d.gdp_per_capita_growth = +d.gdp_per_capita_growth;
-      d.inflation_annual = +d.inflation_annual;
-      d.internet_user_per_100 = +d.internet_user_per_100;
-      d.life_expectancy = +d.life_expectancy;
-      d.military_expenditure_percent_of_gdp = +d.military_expenditure_percent_of_gdp;
-      d.gps_lat = +d.gps_lat;
-      d.gps_long = +d.gps_long;
-    });
-    render(data);
-  });
+  
 
 
 
-  lastSelected = keys[0];
 
 
 }
